55ecae8f8d12138065ca13b67b6e40d538bf78be
ForbiddenR <hostyouposition@outlook.com>
2024-07-22 15:37:54 +0800

    update the downloader

--- http-client/src/bin/downloader.rs
+++ http-client/src/bin/downloader.rs
@@ -2,24 +2,49 @@ use std::{env, error::Error, fs::File, io::Write};
 
 use futures_util::StreamExt;
 use reqwest::{Client, Proxy};
+use serde_json::Value;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let repo_owner = env::var("REPOSITORY").expect("Don't find correct repo owner");
+    let repo_name = env::var("REPOSITORY_NAME").expect("Don't find correct repo");
+
+    let release_url = format!(
+        "https://api.github.com/repos/{}/{}/releases/latest",
+        repo_owner, repo_name
+    );
     // create a client
     let proxy_url = env::var("PROXY_URL").unwrap_or("".into());
 
     let client = if proxy_url.is_empty() {
         reqwest::Client::new()
     } else {
+        println!("use proxy to download file");
         Client::builder().proxy(Proxy::all(proxy_url)?).build()?
     };
 
-    let url = "https://test.com/mimic.tar.gz";
+    let release_info: Value = client
+        .get(&release_url)
+        .header("User-Agent", "Rust Github Release Downloader")
+        .send()
+        .await?
+        .json()
+        .await?;
 
-    let file_name = url
-        .split("/")
-        .last()
-        .expect("cannot get filename from the path");
+    let assets = release_info["assets"].as_array().ok_or("No assets found")?;
+    let asset = assets
+        .into_iter()
+        .find(|x| match x["browser_download_url"].as_str() {
+            Some(url) => url.contains("tar.gz") && url.contains("linux") && url.contains("amd64v3"),
+            None => false,
+        })
+        .ok_or("No valid asset in the url")?;
+    let url = asset["browser_download_url"]
+        .as_str()
+        .ok_or("No download URL found")?;
+    let file_name = asset["name"].as_str().ok_or("No asset name found")?;
+
+    println!("asset url is {} and asset name is {}", url, file_name);
 
     println!("file name is {}", file_name);
 
